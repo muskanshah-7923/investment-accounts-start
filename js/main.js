58f90f6af1f6eda16611f41c538eaecd6e8398fb
mrveldkamp <37160046+mrveldkamp@users.noreply.github.com>
2024-02-05 14:03:38 -0700

    update

--- js/main.js
+++ js/main.js
@@ -4,64 +4,11 @@
 let outputEl = document.getElementById("output");
 
 // Global Variables
-let maxDataVal = 5000; // max data value
+let data = [2000, 4000, 3000, 4500, 1500]; // account data
+let maxVal = 5000; // max data value
 
-// *****************************************************
-// INITIALIZE ACCOUNTS ARRAY
-// *****************************************************
-let accounts = [];
-
-// ******************************************************
-// MENU SELECTION FUNCTIONS
-// ******************************************************
-function deposit() {
-  // Prompt the user for the index of an account and the amount to deposit into that account.
-  // Modify the accounts array to reflect the deposit.
-  // Adjust the maxDataVal variable if necessary.
-  // Use the outputEl to provide a confirmation message.
-
-  outputEl.innerHTML = "Deposit";
-}
-
-function withdrawal() {
-  // Prompt the user for the index of an account and the amount to withdraw from that account.
-  // Modify the accounts array to reflect the withdrawal.
-  // Check to assure that the account has enough funds.
-  // Use the outputEl to provide a confirmation message.
-
-  outputEl.innerHTML = "Withdrawal";
-}
-
-function countUnder2000() {
-  // Count the number of accounts that are less than 2000
-  // Use the outputEl to display the results of the count.
-
-  outputEl.innerHTML = "Count Under $2000";
-}
-
-function generousDonor() {
-  // A generous donor has decided to give $500 to every investment
-  // account that has less than $2000.
-  // Modify the investment account array to apply this donation.
-  // Use the outputEl to display the total amount of money that was donated.
-
-  outputEl.innerHTML = "Generous Donor";
-}
-
-function hackerAttack() {
-  // A hacker steals 5% from every account.
-  // Modify the investment account array to apply this theft.
-  // Use the outputEl to display the total amount that was stolen.
-
-  outputEl.innerHTML = "Hacker Attack";
-}
-
-// ******************************************************
-// END OF MENU SELECTION FUNCTIONS
-// ******************************************************
-
-// Display Data
-drawArray(accounts, maxDataVal);
+// Draw Array every 20ms
+setInterval(drawDataArray, 20);
 
 // Main Menu & Go Button
 document.getElementById("go-btn").addEventListener("click", mainMenu);
@@ -82,18 +29,62 @@ function mainMenu() {
   } else if (selection === "attack") {
     hackerAttack();
   }
-
-  // Redraw array to show any changes
-  drawArray(accounts, maxDataVal);
 }
 
-// DRAW ARRAY FUNCTION
-// Function to draw current state of grades array
-function drawArray(array, maxVal) {
+// ******************************************************
+// MENU SELECTION FUNCTIONS
+// ******************************************************
+function deposit() {
+  // Prompt the user for the index of an account and the amount to deposit into that account.
+  // Modify the data array to reflect the deposit.
+  // Adjust the maxVal variable if necessary.
+  // Use the outputEl to provide a confirmation message.
+
+  outputEl.innerHTML = "Deposit";
+}
+
+function withdrawal() {
+  // Prompt the user for the index of an account and the amount to withdraw from that account.
+  // Modify the data array to reflect the withdrawal.
+  // Check to assure that the account has enough funds.
+  // Use the outputEl to provide a confirmation message.
+
+  outputEl.innerHTML = "Withdrawal";
+}
+
+function countUnder2000() {
+  // Count the number of accounts that are less than 2000
+  // Use the outputEl to display the results of the count.
+
+  outputEl.innerHTML = "Count Under $2000";
+}
+
+function generousDonor() {
+  // A generous donor has decided to give $500 to every investment
+  // account that has less than $2000.
+  // Modify the data array to apply this donation.
+  // Use the outputEl to display the total amount of money that was donated.
+
+  outputEl.innerHTML = "Generous Donor";
+}
+
+function hackerAttack() {
+  // A hacker steals 5% from every account.
+  // Modify the data array to apply this theft.
+  // Use the outputEl to display the total amount that was stolen.
+
+  outputEl.innerHTML = "Hacker Attack";
+}
+
+// ******************************************************
+// END OF MENU SELECTION FUNCTIONS
+// ******************************************************
+
+// Function to draw current state of data array
+function drawDataArray() {
   let outputStr = "";
-  let divHeight;
-  for (let i = 0; i < array.length; i++) {
-    divHeight = (array[i] / maxVal) * 600; // Scale grades to fit in array visualizer container
+  for (let val of data) {
+    let divHeight = (val / maxVal) * 600; // Scale grades to fit in array visualizer container
     outputStr += `<div style="height:${divHeight}px"></div>`;
   }
   document.getElementById("container").innerHTML = outputStr;
